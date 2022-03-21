6333f48ba62f8727aac03c353b93e6ba56ee6be7
Frank Escobedo <frank@techhow.net>
2022-03-21 10:43:29 -0700

    xcuitest copy additional private frameworks xcode13

--- test_runner/xctestrun.py
+++ test_runner/xctestrun.py
@@ -486,7 +486,22 @@ class XctestRunFactory(object):
           uitest_runner_app,
           entitlements_plist_path=entitlements_plist_path,
           identity=test_bundle_signing_identity)
-
+      if xcode_info_util.GetXcodeVersionNumber() >= 1300:
+        _CopyAndSignFramework(
+            os.path.join(
+                platform_path, 'Developer/Library/PrivateFrameworks/'
+                'XCUIAutomation.framework'),
+            runner_app_frameworks_dir, test_bundle_signing_identity)
+        _CopyAndSignFramework(
+            os.path.join(
+                platform_path, 'Developer/Library/PrivateFrameworks/'
+                'XCTestCore.framework'),
+            runner_app_frameworks_dir, test_bundle_signing_identity)
+        _CopyAndSignFramework(
+            os.path.join(
+                platform_path, 'Developer/Library/PrivateFrameworks/'
+                'XCUnit.framework'),
+            runner_app_frameworks_dir, test_bundle_signing_identity)
       bundle_util.CodesignBundle(self._test_bundle_dir)
       bundle_util.CodesignBundle(self._app_under_test_dir)
 
