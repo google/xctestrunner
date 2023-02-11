24629f3e6c0dda397f14924b64eb45d04433c07e
Patrick Balestra <me@patrickbalestra.com>
2023-02-11 18:08:03 +0100

    Update __init__.py
    Co-authored-by: Richard Levasseur <richardlev@gmail.com>

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+# This file is just here to work around Bazel auto-generating an empty file here.
+# What happens is `import xctestrunner` loads this file instead of the
+# subdirectory because the root runfiles directory comes before the subdirectory
+# on sys.path
 import sys
 from . import xctestrunner
 sys.modules['xctestrunner'] = xctestrunner
